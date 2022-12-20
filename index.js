a28dff46c5dfa6bca7968315b2e0648273122040
Laura <lauridangut@gmail.com>
2022-12-20 18:15:27 -0300

    pensando la funcion encriptar

---
+++ index.js
@@ -0,0 +1,5 @@
+function encriptar(){
+    var texto = document.querySelector("#input-texto").value;
+    var textoEncriptado = texto.replace("e","enter").replace("i","imes").replace("a","ai").replace("o","ober").replace("u","ufat");
+    document.querySelector()
+}
