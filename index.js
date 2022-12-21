026eeb34c0d6d1175bc0cfd25fce4019c0470948
Laura <lauridangut@gmail.com>
2022-12-21 16:58:03 -0300

    creacion de una funcion para ocultar elementos del frame 5

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+function ocultar() {
+    var x = document.getElementById("frame-5");
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+  }
+  
+  function ocultarImagen() {
+    document.getElementById("imagen").style.visibility = "hidden";
+  }
+
+  function ocultarFrame() {
+    ocultar();
+    ocultarImagen();
+  }
+
 function encriptar(){
     var texto = document.querySelector("#input-texto").value;
     var textoEncriptado = texto.replace("e","enter").replace("i","imes").replace("a","ai").replace("o","ober").replace("u","ufat");
-    document.querySelector()
+    document.querySelector("#input-texto").value;
 }
+
+var boton1 = 
