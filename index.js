d47eb92d309022b25faef9c4884a84e86ea77a57
Laura <lauridangut@gmail.com>
2023-01-08 11:49:59 -0300

    ocultar muñeco y mensaje cuando se está ingresando texto

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function ocultar() {
     ocultarImagen();
   }
 
-function encriptar(){
+/* function encriptar(){
     var texto = document.querySelector("#input-texto").value;
     var textoEncriptado = texto.replace("e","enter").replace("i","imes").replace("a","ai").replace("o","ober").replace("u","ufat");
     document.querySelector("#input-texto").value;
 }
 
-var boton1 = 
+var boton1 =  */
